7b7da3ef0b4704fd99f4c1807ea07ce473f4f507
Ethan Chandler <86173119+IzzTinkr@users.noreply.github.com>
2021-12-15 22:44:19 -0600

    local storage and timer added, tested, and done

--- assets/script.js
+++ assets/script.js
@@ -35,3 +35,32 @@ function colorCoder() {
 }
 
 colorCoder();
+
+//local storage
+
+$(".saveBtn").on("click", function () {
+  let activityDescription = $(this).siblings(".description").val();
+  let activityTime = parseInt($(this).parent().attr("id"));
+  console.log(activityTime);
+  console.log(activityDescription);
+  if (activityTime < 12) {
+    localStorage.setItem(activityTime + "AM", activityDescription);
+  } else if (activityTime >= 12) {
+    localStorage.setItem(activityTime + "PM", activityDescription);
+  }
+});
+
+//load saved data
+
+$("#9 .description").val(localStorage.getItem("9AM"));
+$("#10 .description").val(localStorage.getItem("10AM"));
+$("#11 .description").val(localStorage.getItem("11AM"));
+$("#12 .description").val(localStorage.getItem("12PM"));
+$("#13 .description").val(localStorage.getItem("13PM"));
+$("#14 .description").val(localStorage.getItem("14PM"));
+$("#15 .description").val(localStorage.getItem("15PM"));
+$("#16 .description").val(localStorage.getItem("16PM"));
+$("#17 .description").val(localStorage.getItem("17PM"));
+
+//timer to continuously check the time and live update
+let timer = setInterval(colorCoder, 60000);
