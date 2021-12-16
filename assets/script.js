f0aa2f49a4e9533bfc491ef96867c429e2d79480
Ethan Chandler <86173119+IzzTinkr@users.noreply.github.com>
2021-12-15 22:18:21 -0600

    color coding script done!

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,30 @@ $(document).ready(function () {
 function colorCoder() {
   let currentHour = moment().hour();
   //add the color coder here
+  $(".hourblock").each(function () {
+    let activityHour = parseInt($(this).attr("id"));
+
+    //if hour is in the past, color it grey
+    if (activityHour < currentHour) {
+      $(this).addClass("past");
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+    }
+
+    //if hour is in the present, color it red
+    else if (activityHour === currentHour) {
+      $(this).addClass("present");
+      $(this).removeClass("future");
+      $(this).removeClass("past");
+    }
+
+    //if hour is in the future, color it green
+    else if (activityHour > currentHour) {
+      $(this).addClass("future");
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+    }
+  });
 }
 
 colorCoder();
